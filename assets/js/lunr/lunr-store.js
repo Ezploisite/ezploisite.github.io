640c85a8cd653b3aff9c1fbd03b72f1354f9e788
Ezploisite <Ezploisite@gmail.com>
2020-04-26 21:47:27 +0200

    Ezploisite

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Joker - Hack The Box",
+        "excerpt":"Joker was a hard box with a Http-Proxy-Squid that allows us through the TFTP to retrieve the username and password. The credentials we retrieve can be used to access the site where, bruteforcing the directories we will find a Python Console. After taking a shell, we will use sudo to...","categories": ["hackthebox"],
+        "tags": ["Joker","Squid-Proxy","Sudo Abuse","SymLink"],
+        "url": "https://ezploisite.it/htb-writeup-joker/",
+        "teaser":"https://ezploisite.it/assets/images/htb-writeup-joker/joker_logo.png"}]
