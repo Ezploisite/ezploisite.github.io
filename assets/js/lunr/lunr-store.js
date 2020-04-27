b5b98dcca44b0bb0bd6f1640887b2d1c4277eb13
Ezploisite <Ezploisite@gmail.com>
2020-04-28 00:37:12 +0200

    Ezploisite

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Joker was a hard box with a Http-Proxy-Squid that allows us through the TFTP to retrieve the username and password. The credentials we retrieve can be used to access the site where, bruteforcing the directories we will find a Python Console. After taking a shell, we will use sudo to...","categories": ["hackthebox"],
         "tags": ["Joker","Squid-Proxy","Sudo Abuse","SymLink"],
         "url": "https://ezploisite.it/htb-writeup-joker/",
-        "teaser":"https://ezploisite.it/assets/images/htb-writeup-joker/joker_logo.png"}]
+        "teaser":"https://ezploisite.it/assets/images/htb-writeup-joker/joker_logo.png"},{
+        "title": "Holiday - Hack The Box",
+        "excerpt":"Holiday was a hard box with a SQL Injection through a post form to retrieve credentials to login. Once gained access, we can stole the admin cookie with an XSS stored in the web page. After that, we can perform command injection through a form, bypassing the regex and obtain...","categories": ["hackthebox"],
+        "tags": ["Holiday","SQL Injection","XSS Stored","Command Injection","Sudo npm"],
+        "url": "https://ezploisite.it/htb-writeup-holiday/",
+        "teaser":"https://ezploisite.it/assets/images/htb-writeup-holiday/holiday_logo.webp"}]
